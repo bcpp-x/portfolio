f6bd2672f501f5046f9786c1751e0eaa5f6389be
Bruna Ceppa <brunaceppa@gmail.com>
2025-04-27 21:19:24 -0300

    add: swiper.js

---
+++ assets/js/sitio/swiper-init.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var swiper = new Swiper(".jsSwiperHome", {
+      direction: "vertical",
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    });
+  });
+  
